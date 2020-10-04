2cff0969a77776c562168308f1e95f0b8de63e03
promsters <tomasz.ptakk@gmail.com>
2020-10-04 11:15:14 +0200

    All tasks done;

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,14 +9,17 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -106,20 +109,28 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int gpa = 0;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
-                case 'F':
-                    return 0;
+                    gpa = 1;
+                    break;
             }
-            return 0;
+
+            if ((studentType == StudentType.DualEnrolled || studentType == StudentType.Honors) && IsWeighted)
+                gpa += 1;
+
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            double studentsAboveInputGrade = 0;
+
+            foreach(Student student in Students)
+            {
+                double studentAverage = 0;
+
+                foreach(double grade in student.Grades)
+                    studentAverage += grade;
+
+                studentAverage /= student.Grades.Count;
+
+                if (studentAverage >= averageGrade)
+                    studentsAboveInputGrade++;
+            }
+
+            double studentsAbovePercentage = studentsAboveInputGrade / Students.Count;
+
+            if (studentsAbovePercentage <= 0.2)
+                return 'A';
+            else if (studentsAbovePercentage <= 0.4)
+                return 'B';
+            else if (studentsAbovePercentage <= 0.6)
+                return 'C';
+            else if (studentsAbovePercentage <= 0.8)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
+    }
+}
